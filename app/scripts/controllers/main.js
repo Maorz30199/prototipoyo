263d89d4497c3cf785a8bfdb85d072a48b39c322
Maorz30199 <cesarruiz@unicauca.edu.co>
2017-03-20 13:33:46 -0500

    Actualización utilizando ui-bootstrap
    Mejora al carousel y estido a imagenes sobre portafolio

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,14 +1,57 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name prototipoyoApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the prototipoyoApp
- */
 angular.module('prototipoyoApp')
-  .controller('MainCtrl', function () {
-    var baseURL = 'http://lorempixel.com/960/450/';
-    $scope.setInterval = 5000;
-  });
+  .controller('MainCtrl', function ($scope) {
+  $scope.myInterval = 5000;
+  var baseURL = 'http://lorempixel.com/960/450/';
+  $scope.noWrapSlides = false;
+  $scope.active = 0;
+  var slides = $scope.slides = [];
+  var contenido = $scope.contenido= {}
+  var currIndex = 0;
+
+  $scope.addSlide = function() {
+    slides.push({
+      title: 'Aprende a mantenerte en forma',
+      image:baseURL+'sports/',
+      text: '¡Practica algún deporte todos los días!',
+      id: currIndex++
+    },
+    {
+      title: 'Buena alimentación',
+      image:baseURL+'food/',
+      text: '¡Es importante de frutas y verduras!',
+      id: currIndex++
+    },
+    {
+      title: 'En contacto con la naturaleza',
+      image:baseURL+'nature/',
+      text: '¡Mantente en armonia con la naturaleza!',
+      id: currIndex++
+    });
+  };
+
+
+  for (var i = 0; i < 1; i++) {
+    $scope.addSlide();
+  }
+
+  baseURL = 'http://lorempixel.com/200/200/';
+  $scope.contenidos =[
+    {
+      img: baseURL+'people/',
+      title: 'Acerca de nosotros',
+      summary: 'Comprometidos con la vida sana'
+    },
+    {
+      img: baseURL+'business/',
+      title: 'Nuestros servicios',
+      summary: 'Asesoria para mantenerte sano'
+    },
+    {
+      img: baseURL+'transport/',
+      title: 'Contáctanos',
+      summary: 'B/sevilla, Medellin'
+    }
+  ]
+});
